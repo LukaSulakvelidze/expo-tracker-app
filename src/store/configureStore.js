5c781e5468a9a348fc4e5df7ec30d843b66bb019
Beka Batmanashvili <62855343+batmanashvili@users.noreply.github.com>
2025-01-21 21:09:33 +0400

    feat: integrate persistor with custom trasnformer

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -10,12 +10,27 @@ import {
   PURGE,
   REGISTER,
   REHYDRATE,
+  createTransform
 } from "redux-persist";
+
+const setTransform = createTransform(
+  // An "inbound" function that gets called right before state is persisted (optional).
+  (inboundState, key) => {
+    return { token: inboundState.token, errorMessage: '' };
+  },
+  // An "outbound" function that gets called right before state is rehydrated (optional).
+  (outboundState, key) => {
+    return outboundState
+  },
+  // define which reducers this transform gets called for.
+  { whitelist: ['auth'] }
+);
 
 const persistConfig = {
   key: "root",
   storage: AsyncStorage,
-  // whitelist: ["token"],
+  whitelist: ["auth"],
+  transforms: [setTransform]
 };
 const persistedReducer = persistReducer(persistConfig, reducer);
 
